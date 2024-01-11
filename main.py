8e735a623f68894a43cbd7948e4bc501b3225c09
Stefanyvitoria <izidiostefany@gmail.com>
2024-01-11 16:05:48 -0300

    WIP: a7a6d75 adicionando modelo com 100 epocas

--- main.py
+++ main.py
@@ -6,31 +6,44 @@ Descrição: Script principal da aplicação.
 
 import cv2, time
 
+from sort.sort import *
+
 # Constantes
-ROOT_PATH = '/home/pi/Documents/TCC/'
+ROOT_PATH = '/mnt/sda1/stefany/tcc/tcc-rep'
 
-# from picamera import PiCamera
+mot_tracker = Sort()
 
-# camera = PiCamera()
-# time.sleep(2)
+# Carregando Modelo
+model_detector_carros = YOLO('yolov8n.pt') # Detectar Carros (Modelo Pre-treinado)
+model_detector_placas = YOLO(f'{ROOT_PATH}/detect/train-100/weights/best.pt') # Placas
 
-# camera.capture("/home/pi/Documents/TCC/midias/img.jpg")
-# print("Done.")
 
-import cv2
-import numpy as np
+veiculos = [2,3,5,7]
 
-cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+# Lendo frames
+frame_num = -1
+ret = True
 
-#set dimensions
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+while ret:
 
-while cap.isOpened():
+    frame_num += 1
+
+    #Ler frames do vídeo
     ret, frame = cap.read()
-    cv2.imwrite('Atharva_Naik.jpg' , frame)
-    cv2.imshow('Video', frame)
-    cv2.waitKey(0)
-       
-cap.release()
-cv2.destroyAllWindows()  
+
+    if ret and frame_num < 10:
+        # Detectando vartices
+        deteccoes = model_detector_carros(frame)[0]
+        # print(deteccao)
+        deteccoes_ = []
+
+        for deteccao in deteccoes.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = deteccao
+            if int(class_id) in veiculos:
+                deteccoes_.append([x1, y1, x2, y2, score])
+
+
+        # rastreia os veiculos
+        track_ids = mot_tracker.update(np.asarray(deteccoes_))
+
+        print(track_ids)
