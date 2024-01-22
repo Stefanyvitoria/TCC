4a4119e8c22a59adad4ac36333d7c40d8073d1d8
Stefanyvitoria <izidiostefany@gmail.com>
2024-01-22 20:32:08 -0300

    separando dataset em duas classes

--- src/utils/formatar_dataset.py
+++ src/utils/formatar_dataset.py
@@ -1,10 +1,16 @@
 import sys, os, shutil
 
-def get_cordenadas(box):
+def get_cordenadas(linha_cordenadas:str):
+    """
+    `linha_cordenadas` - string com cordenadas do retângulo.\n
+    Percorre a linha que contém as cordenadas originais 
+    (linha_cordenadas), limpa e retorna uma lista com as cordenadas 
+    dos 4 cantos do retângulo que comtém o objeto.
+    """
     cordenadas = []
     x = y = ''
     num = 'x'
-    for i in box:
+    for i in linha_cordenadas:
         if i.isnumeric():
             if num == 'x':
                 x = (x + i)
@@ -19,14 +25,27 @@ def get_cordenadas(box):
         else:
             continue
 
-    cordenadas.append((int(x), int(y)))
+    cordenadas.append((int(x), int(y))) # adiciona as ultimas cordenadas
     return cordenadas
 
-def normalize_coordinates(box):
+
+def get_cordenadas_normalizadas(cantos:str):
+    """
+    `cantos` - string com cordenadas dos cantos do retângulo.\n
+    Normaliza as cordenadas de acordo com o modelo esperado
+    no yolov8.
+
+    Retorna uma string com os valores:
+    x_center = (box_x_left+box_x_width/2)/image_width;
+    y_center = (box_y_top+box_height/2)/image_height;
+    width = box_width/image_width;
+    height = box_height/image_height.
+    """
+
     image_width = 1280
     image_height = 720
 
-    cordenadas  = get_cordenadas(box=box)
+    cordenadas  = get_cordenadas(cantos)
 
     box_x_left = cordenadas[0][0]
     box_y_top = cordenadas[0][1]
@@ -41,6 +60,8 @@ def normalize_coordinates(box):
 
 
 def main(path:str):
+
+    print("Processo iniciado.")
     
     # diretórios de destino
     dataset_path = f'{path}/../dataset'
@@ -56,21 +77,29 @@ def main(path:str):
     os.makedirs(images_path_val) if not os.path.exists(images_path_val) else None
     os.makedirs(label_path_val) if not os.path.exists(label_path_val) else None
 
-    # Lê arquivo com divisão
+    # Lê arquivo com divisão/organização do dataset original
     file_split = open(f'{path}/split.txt', 'r')
     file_split_lines = file_split.readlines()
     file_split.close()
 
+    print("Iniciando iteração nos arquivos.")
     for line in file_split_lines:
         line = line.split('/')
 
-        if line[2] not in ['cars-br', 'cars-me']: # ignora placas de moto
+        tipo_placa = line[2]
+
+        if tipo_placa not in ['cars-br', 'cars-me']: # ignora placas de moto
             continue
+        elif tipo_placa == 'cars-br':
+            tipo_placa_id = '0' 
+        elif tipo_placa == 'cars-me':
+            tipo_placa_id = '1'
 
         img_name, img_type = line[-1].split(';')
         label_name = img_name.replace('.jpg', '.txt')
 
-        # Obtém o tipo de imagem (treinamento, teste ou validação)
+        # Define o destino com base no tipo de imagem 
+        #(treinamento, teste ou validação)
         if img_type == 'training\n' or img_type == 'testing\n':
             destino_imagem = images_path_train
             destino_label = label_path_train
@@ -80,28 +109,29 @@ def main(path:str):
         else:
             continue
 
-        # copia a imagem
-        imagem_original = f'{path}/images/{line[2]}/{img_name}'
+        # copia a imagem para o diretório de destino
+        imagem_original = f'{path}/images/{tipo_placa}/{img_name}'
         shutil.copyfile(imagem_original, f'{destino_imagem}/{img_name}')
 
-        # obtém os cantos
+        # obtém os cantos do retângulo que contém a placa
         label_file_dest = open(f"{imagem_original.replace('.jpg', '.txt')}", 'r')
         cantos = label_file_dest.readlines()[-1]
         label_file_dest.close()
 
-        normalized_box = normalize_coordinates(cantos[9:])
+        normalized_box = get_cordenadas_normalizadas(cantos[9:])
 
         # escreve os cantos
         label_file_origin = open(f'{destino_label}/{label_name}', 'w')
-        label_file_origin.write(f'0 {normalized_box}')
+        label_file_origin.write(f'{tipo_placa_id} {normalized_box}')
         label_file_origin.close()
 
+    print(f"Processo finalizado.\nDataset em: {path}/dataset")
 
 
 if __name__ == '__main__':
 
     if (len(sys.argv) <= 1):
-        print("Informe o caminho para a pasta raíz do dataset como primeiro parâmetro.")
+        print("Informe o caminho para a pasta raíz do dataset original como primeiro parâmetro.")
         exit()
     
     if os.path.exists(sys.argv[1]):
