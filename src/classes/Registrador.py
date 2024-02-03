9b3a2aef896bc522172e779b3efa2ab723f6341f
Stefanyvitoria <izidiostefany@gmail.com>
2024-02-03 17:43:32 -0300

    adicionando função de deletar arquivos antigos

--- src/classes/Registrador.py
+++ src/classes/Registrador.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from datetime import date
 from dotenv import load_dotenv
-import os
+import os, arrow
 
 load_dotenv()
 
@@ -9,9 +9,10 @@ class Registrador:
     def __init__(self) -> None:
         self.filename  = ""
 
+        self.delete_old_files()
         self.check_file()
 
-    def check_file(self):
+    def check_file(self) -> None:
         data_atual = date.today()
         self.filename = f"{os.getenv('REGISTROS_PATH')}/Registro-Placas-{data_atual}.xlsx"
 
@@ -20,6 +21,14 @@ class Registrador:
         else:
             Path(self.filename).touch()
             print(f"Arquivo {self.filename} criado com sucesso!")
+
+
+    def delete_old_files(self) -> None:
+        tempo_maximo = arrow.now().shift(days=-int(os.getenv('DIAS_MAX_PERMANENCIA')))
+
+        for item in Path(os.getenv('REGISTROS_PATH')).glob('*'):
+            if item.is_file() and arrow.get(item.stat().st_mtime) < tempo_maximo:
+                item.unlink()
 
 
 if __name__ == "__main__":
